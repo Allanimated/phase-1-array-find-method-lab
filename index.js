99f6fb366ff2135520cb0b81e027a29224c149d1
Allan <allcreativewrites@gmail.com>
2023-09-25 12:44:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(array) {
+    for (const element of array) {
+        if (element.result === "W") {
+            return element.year;
+        }
+    }
+}
